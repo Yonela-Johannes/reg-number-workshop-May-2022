93db22b68c636fdadebdb43c9b99e3990df4705e
Yonela-Johannes <johannesyonela@gmail.com>
2022-06-01 12:57:44 +0200

    registation number using a for loop

--- reg-number.dom.js
+++ reg-number.dom.js
@@ -3,13 +3,20 @@ const regNumber = document.querySelector(".regNumber");
 const regNumbers = document.querySelector(".regNumbers");
 const addRegNumberBtn = document.querySelector(".addRegNumber");
 const message = document.querySelector(".message");
+const storedRegNumbers = []
 
-addRegNumberBtn.addEventListener("click", function() {
-
+addRegNumberBtn.addEventListener("click", function () {
 	if (regNumberEntered.value == "") {
 		message.innerHTML = "Please enter a registration number";
 		return;
 	}
 
-	regNumber.innerHTML = regNumberEntered.value;
+	if (storedRegNumbers.includes(regNumberEntered.value)) {
+		message.innerHTML = "Registration numbers has already been entered";
+	} else {
+		storedRegNumbers.push(regNumberEntered.value)
+		for (let i = 0; i < storedRegNumbers.length; i++) {
+			regNumbers.innerHTML += `<div class='regNumber'>${storedRegNumbers[i]}<div>`
+		}
+	}
 });
